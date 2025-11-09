974594f157f1ad42666f292096a6c97d5df74218
Angie <popeian1@msu.edu>
2025-11-09 21:11:33 +0000

    testing changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addShortcode("entry", function(image, alt, description) {
+    return `<div class="entryflex">
+      <img class="entryimage" src="${image}" alt="${alt}">
+      <p class="entryparagraph">${description}</p>
+    </div>`;
+  });
 };
